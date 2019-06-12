f99d7ed7ff9fe2c0822e4a07cd7a0cac815dfb4b
Rekseto <ReksetoAjxer@gmail.com>
2019-06-12 18:48:09 +0200

    feat: Add actual variable in configuration

--- scrapper/api/models/configurationModel.js
+++ scrapper/api/models/configurationModel.js
@@ -2,12 +2,14 @@ module.exports = function(mongoose) {
   const Schema = mongoose.Schema;
 
   const configurationModel = new Schema({
+    name: {type: String, unique: true},
     maxPrice: {type: Number},
     minPrice: {type: Number},
     city: {type: String},
     placeType: {type: String},
     mailInterval: {type: String},
-    scrapInterval: {type: String}
+    scrapInterval: {type: String},
+    actual: {type: Boolean, required: true}
   });
 
   return {schema: configurationModel, modelName: "configurationModel"};
