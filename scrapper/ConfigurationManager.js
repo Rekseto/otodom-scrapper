c438a9204e18af36bf9ade4e4fa56c4696da0cf1
Rekseto <ReksetoAjxer@gmail.com>
2019-06-12 18:46:25 +0200

    feat: Add ConfigurationManager

---
+++ scrapper/ConfigurationManager.js
@@ -0,0 +1,83 @@
+const defaultConfiguration = {
+  name: "DefaultConfiguration",
+  maxPrice: 250000,
+  minPrice: 150000,
+  city: "poznan",
+  placeType: "flat",
+  mailInterval: "0 6 * * *",
+  scrapInterval: "0 0 * * *",
+  actual: true
+};
+
+class ConfigurationManager {
+  constructor(dependencies) {
+    const {logger, database} = dependencies;
+
+    this.logger = logger;
+    this.database = database;
+
+    this.configuration = null;
+  }
+
+  async createDefaultConfiguration() {
+    const {configurationModel} = this.database.models;
+
+    const exists = await configurationModel.findOne({
+      name: "DefaultConfiguration"
+    });
+    if (exists) return;
+
+    const configuration = await configurationModel.create(defaultConfiguration);
+    return configuration;
+  }
+
+  async initializeConfiguration() {
+    try {
+      const {configurationModel} = this.database.models;
+
+      const configuration = await configurationModel.findOne({
+        actual: true
+      });
+
+      if (configuration) {
+        this.configuration = configuration;
+        return;
+      }
+
+      const defConf = await configurationModel.findOne({
+        name: "DefaultConfiguration"
+      });
+
+      if (defConf) {
+        this.configuration = defConf;
+        return;
+      }
+
+      this.configuration = await this.createDefaultConfiguration();
+    } catch (error) {
+      this.logger.error(error.message);
+      this.configuration = defaultConfiguration;
+    }
+  }
+  async setConfiguration(name) {
+    const nextConfiguration = await configurationModel.find({
+      name
+    });
+
+    if (nextConfiguration) {
+      const actualConfiguration = await configurationModel.find({
+        actual: true
+      });
+
+      actualConfiguration.actual = false;
+      nextConfiguration.actual = true;
+
+      await Promise.all([actualConfiguration.save, nextConfiguration.save]);
+      this.configuration = nextConfiguration;
+    }
+  }
+
+  async createConfiguration(configuration) {}
+}
+
+module.exports = ConfigurationManager;
