93ed4865f8e86e415bb5e4b90ad2c8c7765e76b1
Rekseto <ReksetoAjxer@gmail.com>
2019-06-16 17:32:42 +0200

    feat: Returning cheapest offers on route

--- scrapper/api/routes/offerRoutes.js
+++ scrapper/api/routes/offerRoutes.js
@@ -1,32 +1,20 @@
 const queryString = require("querystring");
 const R = require("ramda");
-const fetch = require("node-fetch");
 
 module.exports = function(router, dependencies) {
   // Services
-  const {logger, services} = dependencies;
+  const {logger, services, configurationManager} = dependencies;
+  const {configuration} = configurationManager;
   const {offersServices} = services;
-  const {fetchOffers} = offersServices;
-
-  //
-  const defaultOfferSearchArgs = {
-    maxPrice: 35000000,
-    minPrice: 25000000,
-    city: "poznan",
-    placeType: "flat"
-  };
 
   router.get("/offers", async (ctx, next) => {
-    const sortedArgs = R.merge(
-      defaultOfferSearchArgs,
-      R.pick(["maxPrice", "minPrice"], queryString.parse(ctx.request.query))
-    );
+    try {
+      const data = await offersServices.getCheapestOffers();
 
-    const data = await fetchOffers(sortedArgs);
-
-    ctx.body = {
-      success: true,
-      data
-    };
+      ctx.body = {
+        success: true,
+        data
+      };
+    } catch (error) {}
   });
 };
